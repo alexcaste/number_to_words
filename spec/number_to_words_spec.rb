7baacc67cfeca1a265c4c73c41bd4950d4325568
james robert williams & alex caste <student@epicodus.com>
2015-04-21 13:36:51 -0700

    add first method spec

---
+++ spec/number_to_words_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('pry')
+
+describe('Fixnum#number_to_words') do
+ #the program converts a single digit number to its word format.
+ it("takes the number and converts it to its word format") do
+  expect(3.number_to_words()).to(eq("three"))
+ end
+end
