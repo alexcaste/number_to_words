bc1d34e4e694cc77d658e1c80d2cad892fb52c95
james robert williams & alex caste <student@epicodus.com>
2015-04-21 15:41:15 -0700

    passed fourth spec.  recoginizes 0 as zero

--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -13,7 +13,17 @@ describe('Fixnum#number_to_words') do
     expect(55.number_to_words()).to(eq("fifty five"))
   end
   #the program converts triple digit numbers into its word format
-  it("convers triple digit number into words") do
+  it("converts triple digit number into words") do
     expect(189.number_to_words()).to(eq("one hundred eighty nine"))
+  end
+
+  #the program prints zero when 0 is entered
+  it("converts 0 to the word zero") do
+    expect(0.number_to_words()).to(eq("zero"))
+  end
+
+  #the program converts quadruple digit numbers into its word format
+  it("converts quadruple digit numbers into words") do
+    expect(5678.number_to_words()).to(eq("five thousand six hundred seventy eight"))
   end
 end
